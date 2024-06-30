21798df3076d3400e534461c9f5d052bd32113ad
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-30 16:59:31 +0200

    Create en.lua

---
+++ Locales/en.lua
@@ -0,0 +1,10 @@
+Locales = {
+    ['weather_menu_title'] = 'Weather and Time Menu',
+    ['weather_option'] = 'Weather',
+    ['time_option'] = 'Time',
+    ['current_time'] = 'Current Time: %02d:%02d',
+    ['weather_changed'] = 'Weather changed to %s',
+    ['time_changed'] = 'Time changed to %02d:%02d',
+    ['weather'] = 'Weather',
+    ['time'] = 'Time'
+}
