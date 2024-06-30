22fc6d11d4f1cba2ef4df4e10356326b85efbfc5
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-30 16:59:09 +0200

    Update cs.lua

--- Locales/cs.lua
+++ Locales/cs.lua
@@ -1,1 +1,10 @@
-
+Locales = {
+    ['weather_menu_title'] = 'Menu Počasí a Čas',
+    ['weather_option'] = 'Počasí',
+    ['time_option'] = 'Čas',
+    ['current_time'] = 'Aktuální čas: %02d:%02d',
+    ['weather_changed'] = 'Počasí bylo změněno na %s',
+    ['time_changed'] = 'Čas byl změněn na %02d:%02d',
+    ['weather'] = 'Počasí',
+    ['time'] = 'Čas'
+}
