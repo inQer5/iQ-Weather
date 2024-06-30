f523e3c88a6a47dd8f49dbd9bb8a011f79aae680
inQer5 <145898779+inQer5@users.noreply.github.com>
2024-06-30 20:42:54 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
 Config = {}
 
 Config.Locale = 'cs'
+
+Config.WeatherTypes = {'CLEAR', 'EXTRASUNNY', 'CLOUDS', 'OVERCAST', 'RAIN'} -- Povolené typy počasí
+
+Config.WeatherChangeInterval = 3600000 -- Interval změny počasí v milisekundách (1 hodina)
